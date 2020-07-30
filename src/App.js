8ed3b10166f6dd3ca36d7b0c8db411a0cdb7f589
aldairbalanzar <abalanzar7@gmail.com>
2020-07-30 06:34:15 -0400

    met mvp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react';
+import gameboy from './media/gameboy_graphic.png'
 import produce from 'immer';
-import './App.css';
+// import './App.css';
+import './App.scss';
 
 function App() {
   const [grid, setGrid] = useState([]);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [speed, setSpeed] = useState(600)
+  const [speed, setSpeed] = useState(500)
   const [genNum, setGenNum] = useState(1)
   const [palette, setPalette] = useState(['#2f5f2e', '#86a70f'])
-  const defaultRows = 32;
-  const defaultCols = 68;
+  const defaultRows = 25;
+  const defaultCols = 25;
   const neighbors = [
     [0,1],
     [0,-1],
@@ -61,6 +63,7 @@ function App() {
     setGrid(rows)
   }
 
+  //clears grid, stops game, and resets genNum to 1
   const handleClear = (rowNum, colNum) => {
     const rows = [];
     rowNum = rowNum ? rowNum : defaultRows
@@ -72,7 +75,7 @@ function App() {
     setGenNum(1)
   }
 
-
+  //Starts game and handles cell living/dying logic
   const startGame = () => {
     if(!isPlayingRef.current) {
       return
@@ -96,7 +99,6 @@ function App() {
             } else if(gridDraft[i][j] === 0 && neighborsNum === 3) {
               gridDraft[i][j] = 1;
             }
-            setGenNum(genNum + 1)
           }
         }
       })
@@ -104,57 +106,104 @@ function App() {
     setTimeout(startGame, speed);
   }
 
+  const genCount = () => {
+    if(isPlayingRef.current){
+      setGenNum(genNum + 1)
+      // console.log(genNum)
+    }
+    return
+  }
+
   //inits our grid
   useEffect(() => {
     setGrid(handleGridSize())
   }, [])
+
+  //rerenders our counter with updated state of genNum
+  useEffect(() => {
+    genCount()
+  }, [grid])
     
-  console.log('GRID: ', grid)
-  console.log(speed)
+  // console.log('GRID: ', grid)
 
   return (
-    <div className="App">
-      <button onClick={() => {
-        setIsPlaying(!isPlaying)
-        if(!isPlaying) {
-          isPlayingRef.current = true;
-          startGame()
-        }}}>
-        {isPlaying ? 'Stop' : 'Start'}
-      </button>
-      <button onClick={() => {
-        setIsPlaying(false)
-        isPlayingRef.current = false;
-        handleClear()
-      }}>
-        Clear
-      </button>
-      <button onClick={()=>{handleRand()}}>Random</button>
-      <button onClick={() => {setSpeed(speed + 100)}}>Slower</button>
-      <button onClick={() => {setSpeed(speed - 100)}}>Faster</button>
-      <button onClick={() => {setPalette(['#2f5f2e', '#86a70f'])}}>GameBoy</button>
-      <button onClick={() => {setPalette(['lightblue', 'pink'])}}>Cotton Candy</button>
-      <p>{genNum}</p>
-      <div style={{
-          display: 'grid',
-          gridTemplateColumns: `repeat(${defaultCols}, 20px)`
-          }}>
-        {grid.map((rows, i) =>
-          rows.map((col, j) => (
-            <div 
-            className="grid"
-            key={`${i}-${j}`}
-            onClick={() => {handleCell(i, j)}}
-            style={{
-              width: 17,
-              height: 15,
-              backgroundColor: grid[i][j] ? palette[0] : palette[1],
-              border: 'solid 1px grey'
-            }}
-            ></div>
-          ))
-          )}
+    <div className="game-container">
+
+      <div className="game-info">
+        <div className="generation-container">
+          <p>Generation: {genNum}</p>
+        </div>
+        <div className="speed-container">
+          <p>Speed: {speed}</p>
+          <button className="game-button" onClick={() => {setSpeed(speed + 50)}}>Slower</button>
+          <button className="game-button" onClick={() => {speed <= 50 ? setSpeed(50) : setSpeed(speed - 50) }}>Faster</button>
+        </div>
       </div>
+
+      <div className="game-container">
+
+        <div className="grid-container">
+          <img className="gameboy" src={gameboy} alt="OG gameboy"/>
+          <div className="grid"
+              style={{
+              display: 'grid',
+              gridTemplateColumns: `repeat(${defaultCols}, 8px)`
+              }}>
+            {grid.map((rows, i) =>
+              rows.map((col, j) => (
+                <div className="cell"
+                key={`${i}-${j}`}
+                onClick={() => {handleCell(i, j)}}
+                style={{
+                  width: 7,
+                  height: 5,
+                  backgroundColor: grid[i][j] ? palette[0] : palette[1],
+                  border: 'solid 1px grey'
+                }}
+                ></div>
+              ))
+              )}
+          </div> 
+        </div>
+
+        <div className="game-rules">
+          <h1>Rules</h1>
+          <ol>
+            <li>If a live cell has less than 2 live neighbors, it'll die</li>
+            <li>If a live cell has more than 3 live neighbors, it'll also die</li>
+            <li>If a live cell has 2-3 live neighbors, it'll keeli living</li>
+            <li>However, if a dead cell has 3 live neighbors, it'll be reborn</li>
+          </ol>
+
+          <div className="button-container">
+            <h3>Controls: </h3>
+            <button className="game-button" onClick={() => {
+              setIsPlaying(!isPlaying)
+              if(!isPlaying) {
+                isPlayingRef.current = true;
+                startGame()
+              }}}>
+              {isPlaying ? 'Stop' : 'Start'}
+            </button>
+            <button className="game-button"
+              onClick={() => {
+              setIsPlaying(false)
+              isPlayingRef.current = false;
+              handleClear()
+            }}>
+              Clear
+            </button>
+            <button className="game-button" onClick={()=>{handleRand()}}>Random</button>
+          </div>
+          <div className="palette-container"> 
+            <h3>Color palette: </h3>
+            <button className="game-button" onClick={() => {setPalette(['#2f5f2e', '#86a70f'])}}>GameBoy</button>
+            <button className="game-button" onClick={() => {setPalette(['lightblue', 'pink'])}}>Cotton Candy</button>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
